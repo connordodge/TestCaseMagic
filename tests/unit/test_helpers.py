de920c5fa507b25ed272f2080750da44db24f8e4
Connor Dodge <connor.dodge@domo.com>
2021-05-14 06:08:08 -0600

    implemented id_is_valid and fixed unittests typo

--- tests/unit/test_helpers.py
+++ tests/unit/test_helpers.py
@@ -14,12 +14,12 @@ class TestIdIsValidHelper(unittest.TestCase):
 
     def test_id_is_valid_string(self):
         is_valid = helpers.id_is_valid("1")
-        self.assertEqual(True, is_valid, "Expected id_is_valid(\"1\") to be False")
+        self.assertEqual(False, is_valid, "Expected id_is_valid(\"1\") to be False")
 
     def test_id_is_valid_zero(self):
         is_valid = helpers.id_is_valid(0)
-        self.assertEqual(True, is_valid, "Expected id_is_valid(0) to be False")
+        self.assertEqual(False, is_valid, "Expected id_is_valid(0) to be False")
 
     def test_id_is_valid_float(self):
         is_valid = helpers.id_is_valid(1.0)
-        self.assertEqual(True, is_valid, "Expected id_is_valid(1.0) to be False")
+        self.assertEqual(False, is_valid, "Expected id_is_valid(1.0) to be False")
